8a018af46a630306e931de1ed3986dd51b19dff7
Mariana Parpinelli de Britto <mariana.dev@hotmail.com>
2024-03-25 21:19:31 -0300

    função do click dos números e botão CE

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,6 +12,9 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        int num1;
+        int num2;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,10 +27,82 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            visor.Text = null;
         }
 
         private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            visor.Text += "0";
+        }
+
+        private void um_Click(object sender, EventArgs e)
+        {
+            visor.Text += "1";
+        }
+
+        private void ponto_Click(object sender, EventArgs e)
+        {
+            visor.Text += ".";
+        }
+
+        private void igual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void adicao_Click(object sender, EventArgs e)
+        {
+            var num1 = visor.Text;
+            visor.Text = null;
+            var num2 = visor.Text;
+        }
+
+        private void dois_Click(object sender, EventArgs e)
+        {
+            visor.Text += "2";
+        }
+
+        private void tres_Click(object sender, EventArgs e)
+        {
+            visor.Text += "3";
+        }
+
+        private void quatro_Click(object sender, EventArgs e)
+        {
+            visor.Text += "4";
+        }
+
+        private void cinco_Click(object sender, EventArgs e)
+        {
+            visor.Text += "5";
+        }
+
+        private void seis_Click(object sender, EventArgs e)
+        {
+            visor.Text += "6";
+        }
+
+        private void sete_Click(object sender, EventArgs e)
+        {
+            visor.Text += "7";
+        }
+
+        private void oito_Click(object sender, EventArgs e)
+        {
+            visor.Text += "8";
+        }
+
+        private void nove_Click(object sender, EventArgs e)
+        {
+            visor.Text += "9";
+        }
+
+        private void divisao_Click(object sender, EventArgs e)
         {
 
         }
